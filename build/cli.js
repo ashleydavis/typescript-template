e4fd8aa642f23214cc3dc1d7707569cc75d4c189
Ashley Davis <ashley@codecapers.com.au>
2017-09-28 19:31:51 +1000

    Updated build scripts.

--- build/cli.js
+++ build/cli.js
@@ -2,4 +2,5 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var index_1 = require("./index");
 (new index_1.ExampleClass()).test();
+//TODO: Code for your command line app goes here. 
 //# sourceMappingURL=cli.js.map
